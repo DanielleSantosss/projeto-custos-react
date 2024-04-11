44851cf9ade358609dc9c400612e54b017523801
Danielle Vitória dos Santos <danielle.vtds@gmail.com>
2024-04-11 18:26:55 -0300

    pages contact e company prontas

--- src/components/pages/Company.jsx
+++ src/components/pages/Company.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 
-function Company(){
-    return(
-        <div><h1>Empresa</h1></div>
-    )
+import styles from './Company.module.css'
+
+function Company() {
+  return (
+    <section className={styles.details}>
+      <h1>Sobre a empresa DSanto's</h1>
+      <article>
+        <p>Olá, muito prazer! <span>Somos a DSanto's (empresa fictícia inventada para fins educacionais).</span></p>
+
+        <p>Estamos comprometidos em fornecer os melhores produtos e serviços para nossos clientes.</p>
+
+        <p>Nossa equipe é formada por profissionais dedicados e apaixonados pelo que fazem, prontos para atender às suas necessidades com excelência.</p>
+
+        <p>Valorizamos a inovação e a criatividade, buscando constantemente novas maneiras de melhorar e aprimorar nossos produtos e processos.</p>
+
+        <p>Estamos aqui para ajudá-lo a alcançar seus objetivos e tornar sua experiência conosco memorável.</p>
+      </article>
+
+      <footer>
+        <p><span>Seja bem-vindo à DSanto's!</span></p>
+      </footer>
+    </section>
+  );
 }
 
 export default Company;
--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 
+import styles from './Contact.module.css'
+
 function Contact(){
     return(
-        <div><h1>Contato</h1></div>
+        <section className={styles.details}>            
+                <h1>Como falar com a D'santos?</h1>           
+            <article>
+                <p><strong>E-mail: </strong>danielle.vtds@gmail.com</p>
+
+                <p><strong>Telefone: </strong>(11) 91111-1111</p>
+
+                <p><strong>GitHub: </strong><a href="https://github.com/DanielleSantosss" target="_blank">GitHub da empresa.</a></p>
+                
+                <p><strong>Linkedn: </strong><a href="https://www.linkedin.com/in/danielle-santos-3936bb1aa/" target="_blank">LinkedIn da empresa.</a></p>
+            </article>
+            
+            <footer>
+                
+            <p>Precisa de alguma ajuda? <span>Não hesite em nos chamar!</span></p>
+
+            </footer>
+        </section>
     )
 }
 
